1ec31a94a8fae210ff5c1a2ca829326ee55afb86
207Lalita <113663388+207Lalita@users.noreply.github.com>
2022-09-22 15:00:48 +0700

    Add files via upload

---
+++ Lab_MatrixMul.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+    public class Lab_MatrixMul {
+    public static void main(String[] args) {
+        int[][] inputA = { { 5, 6, 7 }, { 4, 8, 9 } };
+        int[][] inputB = { { 6, 4 }, { 5, 7 }, { 1, 1 } };
+        MyData matA = new MyData(inputA);
+        MyData matB = new MyData(inputB);
+        int matC_r = matA.data.length;
+        int matC_c = matB.data[0].length;
+        MyData matC = new MyData(matC_r, matC_c);
+
+        // Q4
+        Thread t1 = new Thread(new MatrixMulThread(matC_r, matC_c, matA, matB, matC));
+        t1.start();
+
+        // Q5
+        try {
+            t1.join();
+            } catch (Exception e) { System.out.println(e); }
+
+        matC.show();
+    }
+}
+
+class MatrixMulThread implements Runnable {
+    int processing_row; int processing_col;
+    MyData datA; MyData datB; MyData datC;
+
+    MatrixMulThread(int tRow, int tCol,MyData a, MyData b, MyData c) {
+    // Q1
+        processing_row = tRow;
+        processing_col = tCol;
+        datA = a;
+        datB = b;
+        datC = c;
+    }
+    //Q2
+        public void run() {
+
+    // Q3
+        System.out.println("perform sum of multiplication of assigned row and col");
+        for(int i = 0; i < datA.data.length; i++){
+            for(int j=0; j < datB.data[0].length; j++){
+                datC.data[i][j] = 0;
+                for(int k =0; k<datB.data.length; k++){
+                    datC.data[i][j] += datA.data[i][k]*datB.data[k][j];
+                }
+            }
+        }
+    }
+}
+
+class MyData {
+    int[][] data;
+    MyData(int[][] m) { data = m; }
+    MyData(int r, int c) {
+        data = new int[r][c];
+        for (int[] aRow : data)
+            Arrays.fill(aRow, 9);
+    }
+
+    void show() {
+        System.out.println(Arrays.deepToString(data));
+    }
+}
